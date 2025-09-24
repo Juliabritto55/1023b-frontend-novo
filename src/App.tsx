8384884e2370e5298e4441da2d7290ec20af55ba
Juliabritto55 <julia.britto@estudante.ifms.edu.br>
2025-09-24 11:43:53 -0300

    arrumando codigo da aula

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,54 @@ type ProdutoType = {
 }
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
-
   useEffect(() => {
     fetch('/api/produtos')
       .then((response) => response.json())
       .then((data) => setProdutos(data))
       .catch((error) => console.error('Error fetching data:', error))
   }, [])
+  function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    const data = {
+      nome: formData.get('nome') as string,
+      preco: Number(formData.get('preco')),
+      urlfoto: formData.get('urlfoto') as string,
+      descricao: formData.get('descricao') as string
+    }
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((newProduto) => setProdutos([...produtos, newProduto]))
+      .catch((error) => console.error('Error posting data:', error))
+    form.reset()
+  }
+  function adicionarCarrinho(produtoId: string) {
+    const clienteId = "12345"
+    fetch('/api/carrinho', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ produtoId, clienteId })
+    })
+  }
   return (
     <>
-    <div>Cadastro de Produtos</div>
-    <form onSubmit={handleform}>
-        <input type="text" name="nome" placeholder="Nome do Produto" />
-        <input type="number" name="preco" placeholder="Preço do Produto" />
+      <div>Cadastro de Produtos</div>
+      <form onSubmit={handleForm}>
+        <input type="text" name="nome" placeholder="Nome" />
+        <input type="number" name="preco" placeholder="Preço" />
         <input type="text" name="urlfoto" placeholder="URL da Foto" />
-        <input type="text" name="descricao" placeholder="Descrição do Produto" />
-        <button type="submit">Cadastrar Produto</button>
-    </form>
+        <input type="text" name="descricao" placeholder="Descrição" />
+        <button type="submit">Cadastrar</button>
+      </form>
       <div>Lista de Produtos</div>
       {
         produtos.map((produto) => (
@@ -35,6 +66,7 @@ function App() {
             <p>R$ {produto.preco}</p>
             <img src={produto.urlfoto} alt={produto.nome} width="200" />
             <p>{produto.descricao}</p>
+            <button onClick={()=>adicionarCarrinho(produto._id)}>Adicionar ao carrinho</button>
           </div>
         ))
       }
